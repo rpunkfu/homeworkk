33ee2d1f0a4d81babe5ad99a8fbf86b03709e73e
Alec Jones <alecaej2002@gmail.com>
2016-06-04 16:19:09 -0700

    Added webhook pages and controller stuff

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -1,18 +1,12 @@
-class WebhooksController < ApplicationController
+class HooksController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def receive
-    if request.headers['Content-Type'] == 'application/json'
       data = JSON.parse(request.body.read)
-    else
-      # application/x-www-form-urlencoded
-      data = params.as_json
-    end
 
     Webhook::Received.save(data: data, integration: params[:motion_callback])
 
     render nothing: true
-
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#home'
 
-constraints subdomain: "hooks" do
-  post '/:motion_callback' => 'webhooks#receive', as: :receive_webhooks
+scope '/hooks', :controller => :hooks do
+  post '/:motion_callback' => 'hooks#receive', as: :receive_webhooks
 end
 
   # The priority is based upon order of creation: first created -> highest priority.
