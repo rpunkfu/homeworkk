18f695f0a4d73a020452b34dfec569538eed51b0
Alec Jones <alecaej2002@gmail.com>
2016-06-04 16:39:13 -0700

    deleted test for webhooks

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -8,25 +8,5 @@ class HooksController < ApplicationController
 
     render nothing: true
 
-    context "Motion_callback types, JSON received" do
-
-      before(:each) do
-        request.headers['Content-Type'] = "application/json"
-      end
-
-      let(json_file) { "#{Rails.root}/hooks/motion_callback/exception.json" }
-      let(subject) { post :receive, {integration_name: "motion_callback"} }
-
-      describe "#receive #{event_type}" do
-
-        it "creates a new Motion_callbackWebhook submission" do
-          request.env['RAW_POST_DATA'] = File.read(json_file)
-          data = JSON.parse(File.read(json_file))
-          expect(Webhooks::Received).to receive(:save).with(data: data, integration: "motion_callback").and_return(true)
-          subject
-        end
-      end
-    end
-
-end
+  end
 end
