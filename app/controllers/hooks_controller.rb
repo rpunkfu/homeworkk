ca00b0c0a08b2181dc1b421a777e39f42921639f
Alec Jones <alecaej2002@gmail.com>
2016-06-05 11:04:59 -0700

    trying to fix heroku

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -1,7 +1,5 @@
 class HooksController < ApplicationController
   def receive
-  	data = JSON.parse(request.body.read)
-    Webhook::Received.save(data: data, integration: params[:motion_callback])
-    render nothing: true
+  	
   end
 end
