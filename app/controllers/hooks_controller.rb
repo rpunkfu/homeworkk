d006505e8a824afab14b59a033d81e106892548d
Alec Jones <alecaej2002@gmail.com>
2016-06-05 11:02:34 -0700

    generated controller after deleted it

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -1,10 +1,7 @@
 class HooksController < ApplicationController
-  skip_before_filter :verify_authenticity_token
-
   def receive
-    data = JSON.parse(request.body.read)
+  	data = JSON.parse(request.body.read)
     Webhook::Received.save(data: data, integration: params[:motion_callback])
     render nothing: true
   end
-
 end
--- app/helpers/hooks_helper.rb
+++ app/helpers/hooks_helper.rb
@@ -1,3 +1,2 @@
 module HooksHelper
-
 end
