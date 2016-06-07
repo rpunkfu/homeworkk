f0752f1e30df12fb7410965d484194f5e131df9e
Alec Jones <alecaej2002@gmail.com>
2016-06-07 11:38:13 -0700

    Added hooks controller, routes, and stuff

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -1,4 +1,12 @@
 class HooksController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+
   def receive
-  end
+   data = JSON.parse(request.body.read)
+
+   Webhook::Received.save(data: data, integration: params[:motion_callback])
+
+   render nothing: true
+ end
+
 end
