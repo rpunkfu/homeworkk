76044c1799f5da8964eb62f3ad2f29f42ddb6c6c
Alec Jones <alecaej2002@gmail.com>
2016-06-07 18:12:04 -0700

    Saving, because next part could break

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -2,11 +2,16 @@ class HooksController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def receive
-   data = JSON.parse(request.body.read)
+  	if request.headers['Content-Type'] == 'application/json'
+     data = JSON.parse(request.body.read)
+    else
+      # application/x-www-form-urlencoded
+      data = params.as_json
+    end
 
-   Webhook::Received.save(data: data, integration: params[:motion_callback])
+    Webhook::Received.save(data: data, integration: params[:motion_callback])
 
-   render nothing: true
+    render nothing: true
  end
 
 end
