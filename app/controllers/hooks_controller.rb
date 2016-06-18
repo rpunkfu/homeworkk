f53953e321345179c2ad23ffdbd554ad2ffee256
Alec Jones <alecaej2002@gmail.com>
2016-06-18 12:29:19 -0700

    testing controller receiving

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -3,7 +3,9 @@ class HooksController < ApplicationController
 
   def receive
   	if request.headers['Content-Type'] == 'application/json'
-     data = JSON.parse(request.body.read)
+  		puts "hello"
+  		puts request
+      data = JSON.parse(request.body.read)
     else
       # application/x-www-form-urlencoded
       data = params.as_json
@@ -12,27 +14,5 @@ class HooksController < ApplicationController
     Webhook::Received.save(data: data, integration: params[:motion_callback])
     render nothing: true
 
-    context "Busnag types, JSON received" do
-
-	    before(:each) do
-	      request.headers['Content-Type'] = "application/json"
-	    end
-
-	    let(json_file) { "#{Rails.root}/spec/fixtures/webhooks/bugsnag/exception.json" }
-	    let(subject) { post :receive, {integration_name: "motion_callback"} }
-
-	    # what to write for the following line
-	    describe "#receive #{event_type}" do
-
-	      it "creates a new BugsnagWebhook submission" do
-	        request.env['RAW_POST_DATA'] = File.read(json_file)
-	        data = JSON.parse(File.read(json_file))
-	        expect(Webhooks::Received).to receive(:save).with(data: data, integration: "motion_callback").and_return(true)
-	        subject
-	      end
-	    end
-
-
- 		end
 	end
 end
