06f6c88c11937fa9a15b5d708cd7694aed3dd816
Alec Jones <alecaej2002@gmail.com>
2016-06-20 18:06:45 -0700

    blah blah blah

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -3,18 +3,20 @@ class HooksController < ApplicationController
 
   def receive
 		puts "hello"
-		request
+		puts request
   	if request.headers['Content-Type'] == 'application/json'
       data = JSON.parse(request.body.read)
     else
       # application/x-www-form-urlencoded
       data = params.as_json
     end
-    puts data
 
     Webhook::Received.save(data: data, integration: params[:motion_callback])
     render nothing: true
 
+    $data = data
+    puts "data"
+    puts $data
 	end
 	
 end
