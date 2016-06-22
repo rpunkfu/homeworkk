f4c1e0a291c2bb30adc8eff06fd3821ca7d87bba
Alec Jones <alecaej2002@gmail.com>
2016-06-21 20:24:58 -0700

    testing webhook

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -1,24 +1,16 @@
 class HooksController < ApplicationController
   before_filter :set_webhook_variable
+  before_filter :receive
   skip_before_filter :verify_authenticity_token
-  
-  def tester
-    @number = 123
-  end
 
   def receive
-    puts "hello"
-    puts request
   	if request.headers['Content-Type'] == 'application/json'
       data = JSON.parse(request.body.read)
     else
       # application/x-www-form-urlencoded
       data = params.as_json
     end
-
-    Webhook::Received.save(data: data, integration: params[:motion_callback])
-    render nothing: true
-
+    
     @dataArray.push(data)
   end
 	
