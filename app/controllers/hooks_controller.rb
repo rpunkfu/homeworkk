14d7cbcbc19bdbbe0c66cb36b5eeefd6638f01d2
Alec Jones <alecaej2002@gmail.com>
2016-06-21 18:02:18 -0700

    Testing webhooks

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -1,5 +1,6 @@
 class HooksController < ApplicationController
   skip_before_filter :verify_authenticity_token
+  before_filter :set_webhook_variable
 
   def receive
 		puts "hello"
@@ -14,10 +15,24 @@ class HooksController < ApplicationController
     Webhook::Received.save(data: data, integration: params[:motion_callback])
     render nothing: true
 
-    $tester = 1
+    if !data.nil? 
+      @webhook_variable = data
+      $set_variable_true = 1
+    end
+
 	end
 	
   def test
-    @test = $tester
+    if !@webhook_variable.nil?
+      @data = @webhook_variable
+    end
+  end
+
+  private
+
+  def set_webhook_variable
+    if !$set_variable_true.nil?
+      @webhook_variable = data
+    end
   end
 end
