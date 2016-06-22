ee787d234798e3855008fd64b91705932e63a2c5
Alec Jones <alecaej2002@gmail.com>
2016-06-21 20:06:52 -0700

    testing webhook variables

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -1,10 +1,14 @@
 class HooksController < ApplicationController
-  skip_before_filter :verify_authenticity_token
   before_filter :set_webhook_variable
+  skip_before_filter :verify_authenticity_token
+  
+  def tester
+    @number = 123
+  end
 
   def receive
-		puts "hello"
-		puts request
+    puts "hello"
+    puts request
   	if request.headers['Content-Type'] == 'application/json'
       data = JSON.parse(request.body.read)
     else
@@ -15,24 +19,16 @@ class HooksController < ApplicationController
     Webhook::Received.save(data: data, integration: params[:motion_callback])
     render nothing: true
 
-    if !data.nil? 
-      @webhook_variable = data
-      $set_variable_true = 1
-    end
-
-	end
+    @dataArray.push(data)
+  end
 	
   def test
-    if !@webhook_variable.nil?
-      @data = @webhook_variable
-    end
+    @test = @dataArray
   end
 
   private
 
   def set_webhook_variable
-    if !$set_variable_true.nil?
-      @webhook_variable = data
-    end
+    @dataArray = Array.new
   end
 end
