6676952c05cf48a37849bc6feb7552726ff8c21d
Alec Jones <alecaej2002@gmail.com>
2016-06-23 13:21:12 -0700

    tw

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -9,7 +9,7 @@ class HooksController < ApplicationController
       data = JSON.parse(request.body.read)
     else
       # application/x-www-form-urlencoded
-      data = request.raw_post.read
+      data = request.raw_post
     end
 
     if params[:motion_callback].present?
