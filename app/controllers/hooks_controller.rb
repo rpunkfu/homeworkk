2744204cb0176102e6477527b3ba169d98504299
Alec Jones <alecaej2002@gmail.com>
2016-06-26 10:40:44 -0700

    tw

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -2,15 +2,12 @@ class HooksController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def receive
-    puts "hello"
-  	if request.headers['Content-Type'] == 'application/json'
-      data = JSON.parse(request.body.read)
-    else
-      # application/x-www-form-urlencoded
-      data = request.raw_post
+    if !request.raw_post.nil?
+      puts "hello"
     end
-
-    puts data.inspect
+  	
+    # application/x-www-form-urlencoded
+    data = request.raw_post
 
   end
 	
