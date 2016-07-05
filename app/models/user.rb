eeedf1dc44130145321d7e0c0f4328ce08063df1
Alec Jones <alecaej2002@gmail.com>
2016-07-05 10:11:32 -0700

    testing fb authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ActiveRecord::Base
-	devise :omniauthable, :omniauth_providers => [:facebook]
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
 
   def self.from_omniauth(auth)
 	  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
