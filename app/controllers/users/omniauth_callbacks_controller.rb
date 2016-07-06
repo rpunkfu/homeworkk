6dfde16ffc49e4a6bd4fb336aa5c26164747f616
Alec Jones <alecaej2002@gmail.com>
2016-07-05 21:47:50 -0700

    testing fb authentication

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,4 @@
+def facebook
+  raise request.env['omniauth.auth'].to_yaml
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,11 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-    end
-  end
+      user.first_name = auth.info.first_name
+      user.last_name = auth.info.last_name
+      user.gender = auth.extra.raw.gender
+    end  
+  end 
 end
