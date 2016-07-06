6fcc83f316076e0bc8c27c20d35b7ec3384561b3
Alec Jones <alecaej2002@gmail.com>
2016-07-06 16:32:07 -0700

    restarting this whole facebook login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,6 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
+         :recoverable, :rememberable, :trackable, :validatable, 
          :omniauthable, :omniauth_providers => [:facebook]
-
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
-      user.first_name = auth.info.first_name
-      user.last_name = auth.info.last_name
-      user.gender = auth.extra.raw.gender
-    end  
-  end 
+        
 end
