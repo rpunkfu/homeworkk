86613cfb143b6ccbe1b59c1889eea47e5f32e92c
Alec Jones <alecaej2002@gmail.com>
2016-07-07 09:53:58 -0700

    added skip_before_filter line

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,4 +1,6 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  skip_before_filter :authenticate
+  
   def facebook
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env["omniauth.auth"])
