cf11e6ca2ee3c3633de00615851fcce977d776b3
Alec Jones <alecaej2002@gmail.com>
2016-07-07 11:32:26 -0700

    rolled back previous changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, 
          :omniauthable, :omniauth_providers => [:facebook]
-  
-  auth = session["devise.facebook_data"]
-  
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.password = Devise.friendly_token[0,20]
