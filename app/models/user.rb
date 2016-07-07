3405d38ae23810a83e3300186e9c53217558c74f
Alec Jones <alecaej2002@gmail.com>
2016-07-07 11:08:37 -0700

    tfb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ActiveRecord::Base
          :omniauthable, :omniauth_providers => [:facebook]
         
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create    
+    where(provider: auth.provider, uid: auth.uid).first_or_create
+    user.password = Devise.friendly_token[0,20]
+    user.first_name = auth.info.first_name   # assuming the user model has a name
+    user.image = auth.info.image # assuming the user model has an image    
   end 
 end
