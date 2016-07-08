add730c41319bc9e86463d458fdf233228ba92c2
Alec Jones <alecaej2002@gmail.com>
2016-07-08 12:27:02 -0700

    removed fb authentication

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,13 +3,6 @@ class PagesController < ApplicationController
 
   def home
   	#@messageHuman = Messagehuman.data
-  	@test1 = request.env["omniauth.params"]
-  	@test2 = request.env["rack.session"]["omniauth.origin"]
-  	@test3 = request.env["omniauth.origin"]
-  	@test4 = request.env["omniauth.auth"]
-  	@test5 = session["devise.facebook_data"]
-  	@test6 = session["facebook_data"]
-    @test7 = env["omnauth.auth"]
   end
   
 end
