72decfa1405aae119d76d2d72830addfc7c30c23
Alec Jones <alecaej2002@gmail.com>
2016-07-07 17:50:53 -0700

    tfb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,11 @@ class User < ActiveRecord::Base
          :omniauthable, :omniauth_providers => [:facebook]
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create
-  end 
+  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    user.provider = auth.provider 
+    user.uid = auth.uid
+    user.first_name = auth.info.first_name
+    user.save
+  end
+end
 end
