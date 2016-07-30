d2baed729d5c6657d97da5d4b4e8fc41cfc6a272
Alec Jones <alecaej2002@gmail.com>
2016-07-29 21:04:22 -0700

    testing fb auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,11 @@ class User < ActiveRecord::Base
 
   accepts_nested_attributes_for :groups
 
-  
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
+
 end
