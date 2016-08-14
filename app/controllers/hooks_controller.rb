222b6d8c90ffc5341dacc3b18553fd89944dbd95
Alec Jones <alecaej2002@gmail.com>
2016-08-14 15:48:56 -0700

    added image for fb login

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -8,16 +8,18 @@ class HooksController < ApplicationController
     data = request.raw_post
     $webhook_data = URI::decode_www_form(data)
 
-    if 3 > 2
+    if !$webhook_data.nil?
       @webhook = $webhook_data
-      #$doesUserExist = User.where("conversation_id = ?", @webhook[0][1])
-      Messagehuman.message(@webhook[0][1], "Sign up <a href='https://christopherbot.herokuapp.com'>here</a>")
+      @doesUserExist = User.where("conversation_id = ?", @webhook[0][1])
+
+      if @doesUserExist.nil?
+        Messagehuman.message(@webhook[0][1], "Sign up at https://christopherbot.herokuapp.com/users/sign_in")
+      end
     end
   end
 	
   def test
      @webhook = $webhook_data # used to inspect the webhook data 
-  
   end
 
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def home
- 
+ 		#Messagehuman.message("134381003642835", "Sign up" + <a href='https://christopherbot.herokuapp.com'>here</a> )
   end
   
 end
