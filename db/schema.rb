df143c20a8b3cadcb6d45deba48b78aa9a3447d5
Alec Jones <alecaej2002@gmail.com>
2016-08-14 09:12:41 -0700

    removed email from users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160807214416) do
+ActiveRecord::Schema.define(version: 20160814161052) do
 
   create_table "groups", force: :cascade do |t|
     t.integer "fb_id"
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(version: 20160807214416) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -44,7 +43,6 @@ ActiveRecord::Schema.define(version: 20160807214416) do
     t.string   "conversation_id"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
