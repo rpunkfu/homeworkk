25bfe07490b859d138ed52290d704db5fe43ae8e
Alec Jones <alecaej2002@gmail.com>
2016-08-21 13:56:11 -0700

    testing out code that redirects user back after sign-in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,21 @@ class ApplicationController < ActionController::Base
   def set_user_api_token
     RequestStore.store[:my_api_token] = current_user.api_token # or something similar based on `session`
   end
+
+  # If your model is called User
+	def after_sign_in_path_for(resource)
+	  session["user_return_to"] || root_path
+	end
+
+	# Or if you need to blacklist for some reason
+	def after_sign_in_path_for(resource)
+	  blacklist = [new_user_session_path, new_user_registration_path] # etc...
+	  last_url = session["user_return_to"]
+	  if blacklist.include?(last_url)
+	    root_path
+	  else
+	    last_url
+	  end
+	end
+
 end
