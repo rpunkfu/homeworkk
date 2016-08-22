5f1f83c9e89beb82f6be0377dd41a6dd4c2afdcf
Alec Jones <alecaej2002@gmail.com>
2016-08-21 17:55:16 -0700

    testing out this code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
 	end
 
 	def after_sign_in_path_for(resource)
-	  session[:previous_url] || root_path
+	  session[:previous_url]
 	end
 
   protected
