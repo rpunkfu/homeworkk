7217e75192cffae5b23532be35b7c6803b569257
Alec Jones <alecaej2002@gmail.com>
2016-08-21 18:33:33 -0700

    added conversation id

--- app/controllers/users/sessions_controllers.rb
+++ app/controllers/users/sessions_controllers.rb
@@ -1,8 +1,5 @@
 class Users::SessionsController < Devise::SessionsController
   def new
-  	$param1 = params[:conversation_id] if !params[:conversation_id].nil?
-  	if user_signed_in?
-    	redirect_to root_url(param1)
-  	end
+  	$conversation_id = params[:conversation_id] if !params[:conversation_id].nil?
   end
 end
