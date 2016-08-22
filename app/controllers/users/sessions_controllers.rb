108e8dce79a6087d9cff7356c8beed90d6f4e77f
Alec Jones <alecaej2002@gmail.com>
2016-08-21 18:27:25 -0700

    trying to fix it

--- app/controllers/users/sessions_controllers.rb
+++ app/controllers/users/sessions_controllers.rb
@@ -1,6 +1,6 @@
 class Users::SessionsController < Devise::SessionsController
   def new
-  	param1 = params[:conversation_id] if !params[:conversation_id].nil?
+  	$param1 = params[:conversation_id] if !params[:conversation_id].nil?
   	if user_signed_in?
     	redirect_to root_url(param1)
   	end
