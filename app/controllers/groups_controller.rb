51d6e3ce58680fb783107efbebf44353d87da4ce
Alec Jones <alecaej2002@gmail.com>
2016-08-21 18:48:17 -0700

    fixed user update bug

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,7 +8,7 @@ class GroupsController < ApplicationController
     @groups = Group.all
     if !$conversation_id.nil?
       @user = User.where("id = ?", current_user.id)
-      @user = User.update(conversation_id = $conversation_id)
+      @user.update(conversation_id = $conversation_id)
     end
   end
 
