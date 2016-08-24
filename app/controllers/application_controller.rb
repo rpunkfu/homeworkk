0bff1b07c1ded455103d95a17730ae0291b2cf3b
Alec Jones <alecaej2002@gmail.com>
2016-08-24 09:45:12 -0700

    trying to fix fb error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,21 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   after_filter :store_location
-=begin
-  def store_location
-	  # store last url as long as it isn't a /users path
-	  session[:previous_url] = request.fullpath unless request.fullpath =~ /\/users/
-	end
-=end
-
-	def after_sign_up_path_for(resource)
-    root_path 
-    return request.env['omniauth.origin'] || session[:return_to] 
-  end 
-
-  def after_sign_in_path_for(resource)
-    return request.env['omniauth.origin'] || session[:return_to]
-  end
 
   protected
   def set_user_api_token
