9bfdd4c3e7b78039e3c3c1eaf54d1bb5d3f8546b
Alec Jones <alecaej2002@gmail.com>
2016-09-10 19:20:56 -0700

    trying to get JSOn replys working

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -7,17 +7,21 @@ class HooksController < ApplicationController
     # application/x-www-form-urlencoded
     data = request.raw_post
     $webhook_data = URI::decode_www_form(data)
+    @webhook = $webhook_data
 
+    respond_to do |format|
+      format.json { { "value1": "alec", "value2": "jones" } }
+    end
+=begin
     if !$webhook_data.nil?
-
-      @webhook = $webhook_data
       $doesUserExist = User.where("conversation_id = ?", @webhook[0][1])
 
       if $doesUserExist.empty?
        Messagehuman.message(@webhook[0][1], "Sign up at https://christopherbot.herokuapp.com/users/sign_in?conversation_id=#{@webhook[0][1]}")
       end
     end
-end
+=end
+  end
 	
   def test
      @webhook = $webhook_data # used to inspect the webhook data 
