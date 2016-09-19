4c8f4f0cf080f9791a293adc31496220ee32db6b
Alec Jones <alecaej2002@gmail.com>
2016-09-18 17:49:26 -0700

    decoded form data

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -1,7 +1,7 @@
 # YOUR_APP/app/controllers/messenger_controller.rb
 class MessengerController < Messenger::MessengerController
   def webhook
-  	$webhook = request.raw_post
+  	$webhook = URI::decode_www_form(request.raw_post)
     #logic here
     render nothing: true, status: 200
   end
