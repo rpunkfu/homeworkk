f9d91459e9ce7972859a85a46070b6a6b6c5b3ff
Alec Jones <alecaej2002@gmail.com>
2016-09-25 09:50:27 -0700

    trying to send a message

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -1,9 +1,11 @@
-# YOUR_APP/app/controllers/messenger_controller.rb
 class MessengerController < Messenger::MessengerController
 	require 'json'
   def webhook
   	$webhook = JSON.parse(request.raw_post)
+
     #logic here
+    Messenger::Request.new("hello, how are you?", 134381003642835)
+
     render nothing: true, status: 200
   end
 
