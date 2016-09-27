26209a3c0a2e6d8e13a87ae9652f17b95689a5c4
Alec Jones <alecaej2002@gmail.com>
2016-09-26 17:29:58 -0700

    wrong logic

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -5,7 +5,7 @@ class MessengerController < Messenger::MessengerController
 
     #logic here
   	if fb_params.first_entry.callback.message?
-  		if User.where("conversation_id = ?", fb_params.first_entry.sender_id) == true
+  		if User.where("conversation_id = ?", fb_params.first_entry.sender_id).empty?
   			text = "https://christopherbot.herokuapp.com/users/sign_in?conversation_id=#{fb_params.first_entry.sender_id}"
 	  		messageText = Messenger::Elements::Text.new(text: text), fb_params.first_entry.sender_id
 	  		
