ff0937905b773fbb473c78ca2a9f5c7aff2e0e8e
Alec Jones <alecaej2002@gmail.com>
2016-10-01 09:46:30 -0700

    trying to get forms to work

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,7 +7,7 @@ class GroupsController < ApplicationController
 
   def index
     @params = params
-    @class_number_user = Hash.new(:class_number => params)
+    @class_number_user = Array.new("form-value")
 
 
     @groups = Group.all
