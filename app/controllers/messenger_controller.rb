7e8fa3c0501c135517ca5df80fc9ac9d36d72f20
Alec Jones <alecaej2002@gmail.com>
2016-10-01 18:41:08 -0700

    send message

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -6,7 +6,6 @@ class MessengerController < Messenger::MessengerController
 
     #logic here
   	if fb_params.first_entry.callback.message?
-      $userthing = fb_params.first_entry.callback.message?
   		if User.find_by(conversation_id: fb_params.first_entry.sender_id).empty?
   			text = "https://christopherbot.herokuapp.com/users/sign_in?conversation_id=#{fb_params.first_entry.sender_id}"
   		end
@@ -14,7 +13,7 @@ class MessengerController < Messenger::MessengerController
   	  
       Messenger::Client.send(
         Messenger::Request.new(
-          Messenger::Elements::Text.new(text: text),
+          Messenger::Elements::Text.new(text: "hi"),
           fb_params.first_entry.sender_id
         )
       )
