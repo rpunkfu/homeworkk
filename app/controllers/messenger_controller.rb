72d58175a3323788439f391ffaa9609c351db302
Alec Jones <alecaej2002@gmail.com>
2016-10-01 17:22:35 -0700

    sender user message

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -3,6 +3,16 @@ class MessengerController < Messenger::MessengerController
   def webhook
   	$webhook = JSON.parse(request.raw_post)
 
+    #logic here
+  	if fb_params.first_entry.callback.message?
+  		if User.where("conversation_id = ?", fb_params.first_entry.sender_id).empty?
+  			text = "https://christopherbot.herokuapp.com/users/sign_in?conversation_id=#{fb_params.first_entry.sender_id}"
+  		end
+  		
+  		sendUserMessage(text, fb_params.first_entry.sender_id)
+		end
+
+    render nothing: true, status: 200
   end
 
   def webhook_inspect
