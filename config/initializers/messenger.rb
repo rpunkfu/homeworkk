140de61fd04a691b6fca57b9bda5c0f6943f822c
Alec Jones <alecaej2002@gmail.com>
2016-10-01 18:43:27 -0700

    set new page access token

--- config/initializers/messenger.rb
+++ config/initializers/messenger.rb
@@ -1,4 +1,4 @@
 Messenger.configure do |config|
   config.verify_token      = '1234' #will be used in webhook verifiction
-  config.page_access_token = 'EAAZAjj9YZAiZC0BACZB9rTOVlMEk1MrjTJ3xqZCCjYcTLKayBfonTAZC7JaZB9dJoNxGr5w0cyOALL5VzQ7zMREbZBFxd3xQ6lqtDm1vY2g3kzVUcsv8EjPtQRjlpYC36gnEVWpzF5kgqLfBGBctvyrZAs9keJKqGwy7cwPCZBa6v7ZCgZDZD'
+  config.page_access_token = 'EAAZAjj9YZAiZC0BAOqtKTpEHNhhcMGS0gd5EllDfucbiRmqouGQqygtAGzTOF73ZCWRf1dFMRRTj1bmUxnv3wbyH26ysJc4rnMDWZAzlQY8aEgC7PRAX1ilY3oZBCiQ0ZAihrNIRPh6niU3yWPzTSijMDaloEGhyvZBblD2DKNopKQZDZD'
 end
