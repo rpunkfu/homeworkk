a33444c90d7d54ebb2fe25118e4c6c4242582d18
Alec Jones <alecaej2002@gmail.com>
2016-10-01 18:20:34 -0700

    send user message

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -10,7 +10,14 @@ class MessengerController < Messenger::MessengerController
   			text = "https://christopherbot.herokuapp.com/users/sign_in?conversation_id=#{fb_params.first_entry.sender_id}"
   		end
   		
-  	  sendUserMessage(text, fb_params.first_entry.sender_id)
+  	  
+      Messenger::Client.send(
+        Messenger::Request.new(
+          Messenger::Elements::Text.new(text: text),
+          fb_params.first_entry.sender_id
+        )
+      )
+
 		end
 
     render nothing: true, status: 200
