30cbebac3ad2e28ac738af5b406223b3138e63ad
Alec Jones <alecaej2002@gmail.com>
2016-10-08 11:54:57 -0700

    setting up facebook messenger on http protocols only

--- config/initializers/messenger.rb
+++
@@ -1,4 +0,0 @@
-Messenger.configure do |config|
-  config.verify_token      = '1234' #will be used in webhook verifiction
-  config.page_access_token = 'EAAZAjj9YZAiZC0BAGnqnIGKtIO6ZAnufavGlBIvRIjrTaI0u6gEkbcIqnPpZCTbzzLyDAE8cRDBkWftLTwHxJigs5GZBZAbJgrEhTHqrIiV2BfrMOCGiZABXl46zK9kVPcqKOl0pZA1XAL3WlnfgDQ2vTTQFgs2ZC6dDsg3QZCicmjLygZDZD'
-end
