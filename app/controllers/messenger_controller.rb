add4957d9c0a8839f76f7a95c4c9c9629b430422
Alec Jones <alecaej2002@gmail.com>
2016-10-08 11:44:16 -0700

    switching to all HTTP protocols

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -4,18 +4,7 @@ class MessengerController < Messenger::MessengerController
 
   def webhook
   	$webhook = JSON.parse(request.raw_post)
-
-    #logic here
-  	if fb_params.first_entry.callback.message?
-  		if User.find_by(conversation_id: fb_params.first_entry.sender_id).nil?
-  			text = "https://christopherbot.herokuapp.com/users/sign_in?conversation_id=#{fb_params.first_entry.sender_id}"
-      else
-        text = "hello, you've already signed up, can't wait to show you what I can do in the future"
-  		end
     
-      sendMessage()
-
-		end
 
     render nothing: true, status: 200
   end
