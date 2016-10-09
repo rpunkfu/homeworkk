11b95461c75a680fcb4c70aa9fb2f5de0f2523a1
Alec Jones <alecaej2002@gmail.com>
2016-10-09 10:16:49 -0700

    testing

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -7,7 +7,21 @@ class MessengerController < ApplicationController
  	$webhook = JSON.parse(request.raw_post)
  	page_access_token = 'EAAZAjj9YZAiZC0BAFZCao4ZADSMy9o60qDLr2y8zvB14OElmfuXyNq6LbjRwwSPAptK9eNGcHI73VLKaTrk5R6nanUa7mFJPD0Rp5p0p0ZBPOanIRkZABOX9ZC590Q5WfNAABPlRwf1GmWRhhtxnMgeOZBcylDMCrpwjXOS1NBaTDwwZDZD'
 
- 	
+1.times do 
+ 	body = {
+ 		recipient: {
+   		id: '134381003642835'
+ 		},
+ 		message: {
+   		text: 'text'
+ 		}
+	}.to_json
+	response = HTTParty.post(
+ 		"https://graph.facebook.com/v2.6/me/messages?access_token=#{page_access_token}",
+ 		body: body,
+ 		headers: { 'Content-Type' => 'application/json' }
+	)
+end
  end
 
  def webhook_inspect
