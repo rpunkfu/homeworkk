c1ae39c32b4271948c41d5a4c95c6037b43818c9
Alec Jones <alecaej2002@gmail.com>
2016-10-09 10:02:47 -0700

    inspecting variables

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -5,6 +5,7 @@ class MessengerController < ApplicationController
  def receive_message
  	checkFacebookToken() # checks to make sure that the token from facebook is correct
  	$webhook = request.raw_post
+ 	$webhook1 = params
  end
 
  def webhook_inspect
