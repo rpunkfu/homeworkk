4b4b08d9b4c7098a3caa32cbef905038a6ea4f9d
Alec Jones <alecaej2002@gmail.com>
2016-10-09 09:52:10 -0700

    trying to get webhook to work

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -1,17 +1,16 @@
 class MessengerController < ApplicationController
 	require 'json'
 
-  def webhook
-   if params['hub.verify_token'] == '123456789'
+
+
+ def receive_message
+ 	if params['hub.verify_token'] == '123456789'
    		puts "this is successful"
      render text: params['hub.challenge'] and return
    else
    	puts "this is not good"
      render text: 'ahhhhhhh, error' and return
    end
- end
-
- def receive_message
  	$webhook = request.raw_post
  end
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root 'groups#index'
 
   post 'messenger/webhook' => 'messenger#receive_message'
-  get 'messenger/webhook' => 'messenger#webhook'
+
 
 
 
