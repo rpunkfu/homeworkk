6095b73bdaeeaf655e088bc2bf633c23e84e13ed
Alec Jones <alecaej2002@gmail.com>
2016-10-10 16:10:27 -0700

    trying to fix fb

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -3,8 +3,12 @@ class MessengerController < ApplicationController
 	include MessengerHelper
 
 	def receive_message
- 		checkFacebookToken() # checks to make sure that the token from facebook is correct
+ 		
  		$webhook = JSON.parse(request.raw_post)
+ 	end
+
+ 	def webhook
+ 		checkFacebookToken()
  	end
 
  	def webhook_inspect
