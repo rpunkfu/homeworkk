52b41b13e55337397b0b1584bdd26be36030c51b
Alec Jones <alecaej2002@gmail.com>
2016-10-10 15:44:44 -0700

    testing

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -6,12 +6,10 @@ class MessengerController < ApplicationController
  		checkFacebookToken() # checks to make sure that the token from facebook is correct
  		$webhook = JSON.parse(request.raw_post)
  		puts "webhook page has loaded"
- 		puts "1." + $webhook[:entry][:messaging][:id]
- 		puts "2." + $webhook['entry']['messaging']['id']
-
- 		if User.find_by(conversation_id: $webhook[:entry][:messaging][:id]).blank?
+ 		
+ 		if !User.find_by(conversation_id: $webhook['entry']['messaging']['id']).blank?
  			#sendUserMessage('134381003642835', "hi alec")
- 			puts "1. worked"
+ 			puts "worked"
  		end
  	end
 
