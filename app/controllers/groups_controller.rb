df6e82e990336d147f3c388acf54b0e32871921b
Alec Jones <alecaej2002@gmail.com>
2016-10-13 16:24:44 -0700

    testing out group forms

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,7 +6,7 @@ class GroupsController < ApplicationController
   # GET /groups.json
 
   def index
-    if user_signed_in? && current_user.class_number.nil?
+    if user_signed_in? && current_user.class_number.to_i.nil?
       @setUserClassNumber = User.find_by(id: current_user.id)
       @setUserClassNumber.update(class_number: "5")
       @setUserClassNumber.save 
