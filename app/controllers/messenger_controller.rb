e1590281176e9a043d6f37e02cdc47278c56357d
Alec Jones <alecaej2002@gmail.com>
2016-10-12 20:08:35 -0700

    testing out stiff

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -8,14 +8,14 @@ class MessengerController < ApplicationController
  		$webhook = JSON.parse(request.raw_post)
  		$webhook[1][1].each do |key, value|
 			key["messaging"].each do |key, value|
-				@sender_id = key["sender"]["id"]
+				$the_sender_id = key["sender"]["id"]
 	 		end
 		end
- 		sendUserMessage(@sender_id, "how you doin")
+ 		sendUserMessage($the_sender_id, "how you doin")
  	end
 
  	def webhook_inspect
- 		@test = {"object"=>"page", "entry"=>[{"id"=>"1135063803224611", "time"=>1476317596353, "messaging"=>[{"sender"=>{"id"=>"134381003642835"}, "recipient"=>{"id"=>"1135063803224611"}, "timestamp"=>1476317596288, "message"=>{"mid"=>"mid.1476317596288:617e906092", "seq"=>2771, "text"=>"hi"}}]}]}.to_a
+ 		
  	end
  
 end
