d0861d00701507dfcb886f30cd68c13f3f2d90ab
Alec Jones <alecaej2002@gmail.com>
2016-10-12 17:47:33 -0700

    testing out sending a message

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -6,10 +6,16 @@ class MessengerController < ApplicationController
 	def receive_message
 		checkFacebookToken()
  		$webhook = JSON.parse(request.raw_post)
- 		sendUserMessage()
+ 		$webhook[1][1].each do |key, value|
+			key["messaging"].each do |key, value|
+				@sender_id = key["sender"]["id"]
+	 		end
+		end
+ 		sendUserMessage(@sender_id, "how you doin")
  	end
 
  	def webhook_inspect
+ 		@test = {"object"=>"page", "entry"=>[{"id"=>"1135063803224611", "time"=>1476317596353, "messaging"=>[{"sender"=>{"id"=>"134381003642835"}, "recipient"=>{"id"=>"1135063803224611"}, "timestamp"=>1476317596288, "message"=>{"mid"=>"mid.1476317596288:617e906092", "seq"=>2771, "text"=>"hi"}}]}]}.to_a
  	end
  
 end
--- app/helpers/messenger_helper.rb
+++ app/helpers/messenger_helper.rb
@@ -8,21 +8,23 @@ module MessengerHelper
   	end
 	end
 
-	def sendUserMessage()
-		access_token = "EAAZAjj9YZAiZC0BAHofK78pYfkKG7DRUvKnGraNZAHmVi8vzq9NdGSZBbO15rgvu0ubZAu7YXGCHpoGeUqcmtKFDT3OYOZCFSIVAWmylmGmxriUts1DVtMzpXmVMJeSZAshRRoIy293BwnLkLqz8d0Owq4nhLB0oKvMbyaDqeMZBHnwZDZD"
-		url = "https://graph.facebook.com/v2.6/me/messages?access_token=" + access_token
-		sender_id = 134381003642835
-		message = "connor eats pizza"
+	def sendUserMessage(senderId, definedMessage)
+		if !$webhook.nil?
+			access_token = "EAAZAjj9YZAiZC0BAHofK78pYfkKG7DRUvKnGraNZAHmVi8vzq9NdGSZBbO15rgvu0ubZAu7YXGCHpoGeUqcmtKFDT3OYOZCFSIVAWmylmGmxriUts1DVtMzpXmVMJeSZAshRRoIy293BwnLkLqz8d0Owq4nhLB0oKvMbyaDqeMZBHnwZDZD"
+			url = "https://graph.facebook.com/v2.6/me/messages?access_token=" + access_token
+			sender = senderId
+			message = definedMessage
 
-		options = {
-        body: {
-          recipient: {id: sender_id},
-          message: {text: message}
-        }
-		}
+			options = {
+	        body: {
+	          recipient: {id: sender},
+	          message: {text: message}
+	        }
+			}
 
-		result = HTTParty.post(url, options)
-		print result
+			result = HTTParty.post(url, options)
+			print result
+		end
 	end
 
 end
