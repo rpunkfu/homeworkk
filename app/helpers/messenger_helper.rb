867d8631363580129ea54a5f7c76437b8b7ec671
Alec Jones <alecaej2002@gmail.com>
2016-10-12 20:31:29 -0700

    stuff

--- app/helpers/messenger_helper.rb
+++ app/helpers/messenger_helper.rb
@@ -8,11 +8,11 @@ module MessengerHelper
   	end
 	end
 
-	def sendUserMessage(senderId)
+	def sendUserMessage()
 		if !$webhook.nil?
 			access_token = "EAAZAjj9YZAiZC0BAHJ2zRhGWF8FgzAu8NTWCwD7tQ3IsrH1gVsDiMzJgFD6kyFHY6BuCK5XyorlFGHg3H3tvZBiNRUuAJg3ZCtYDMTts2ZC4leEcYhZC6rBuGc6rZCTxckz0xUOPSqIQn6fxHvJJxMiyk7mbGvP4KoAIOXwgZA6hCkwZDZD"
 			url = "https://graph.facebook.com/v2.6/me/messages?access_token=" + access_token
-			sender = senderId
+			sender = 134381003642835
 			message = 'how you doin'
 
 			options = {
