81509afa494e2996c43db7e7d262877d1add6e56
Alec Jones <alecaej2002@gmail.com>
2016-10-12 20:10:56 -0700

    sending user message

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -6,12 +6,14 @@ class MessengerController < ApplicationController
 	def receive_message
 		checkFacebookToken()
  		$webhook = JSON.parse(request.raw_post)
+=begin
  		$webhook[1][1].each do |key, value|
 			key["messaging"].each do |key, value|
 				$the_sender_id = key["sender"]["id"]
 	 		end
 		end
- 		sendUserMessage($the_sender_id, "how you doin")
+=end 
+ 		sendUserMessage(134381003642835, "friends is the ultimate show")
  	end
 
  	def webhook_inspect
