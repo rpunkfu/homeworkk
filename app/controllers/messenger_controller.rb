484baf64bbe77788fa156bf7fde63c84bdf4b253
Alec Jones <alecaej2002@gmail.com>
2016-10-13 19:07:53 -0700

    fixed it

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -21,7 +21,7 @@ class MessengerController < ApplicationController
 	   body: body,
 	   headers: { 'Content-Type' => 'application/json' }
 	  )
-	  render nothing => true, status: 200
+	  render :nothing => true, status: 200
  	end
 
  	def sendtoken
