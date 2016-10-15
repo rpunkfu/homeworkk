ada72ecb69fb1b5ddd665038806fe45d078a6451
Alec Jones <alecaej2002@gmail.com>
2016-10-15 13:59:52 -0700

    trying to fix webhook

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -5,7 +5,7 @@ class MessengerController < ApplicationController
 
 	def receive_message
  		$webhook = JSON.parse(request.raw_post)
- 		page_access_token = 'EAAZAjj9YZAiZC0BALdZB6lBltZAenVPyLZBDN5ZBD0NecQBZCBHYIkH2eF7MvGgomp8ZB4ZCqgnZAqqZAYIpRqsWf0HT8H1MHjgAGUZAsK45r6GAqgBb5jZA33BGBEZBTNhJND880K76nafD4AtmuZBNPF0yzZAN1MUZCjL7fGZCzLBkoL02jlX4gZDZD'
+ 		page_access_token = 'EAAZAjj9YZAiZC0BALu16ZCAkRt33hbnZAqeZBSJgBqjTiEjYz9eP2InIpwbolx7xe8bhfJ9o6oLRTGId6oZCBAbUMoFGgYlZCGVT2NbIcq3dkVAdgw5jh3ZCIT9CDONkx5QSp7nlRcrX5HncCAKwZBBft8UjitoLMgDJXNul7dIzlfbgZDZD'
 
 	  body = {
 	   recipient: {
@@ -19,7 +19,7 @@ class MessengerController < ApplicationController
 	  response = HTTParty.post(
 	   "https://graph.facebook.com/v2.6/me/messages?access_token=#{page_access_token}",
 	   body: body,
-	   headers: { 'Content-Type' => 'application/json' }
+	   headers: { 'Content-Type' => 'application/json', 'status' => 200}
 	  )
 	  render :nothing => true, status: 200
  	end
