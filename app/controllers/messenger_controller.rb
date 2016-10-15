5c463a833dab598964176e0a00b9562d703469ad
Alec Jones <alecaej2002@gmail.com>
2016-10-15 14:42:49 -0700

    added status ok

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -20,6 +20,7 @@ class MessengerController < ApplicationController
 	   "https://graph.facebook.com/v2.6/me/messages?access_token=#{page_access_token}",
 	   body: body,
 	   headers: { 'Content-Type' => 'application/json'},
+	   status: 'ok'
 	  )
 	  render status: 'ok'
  	end
