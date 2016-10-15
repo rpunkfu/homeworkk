505a170bdb3c3393d0f4cda771aca404b6bf97c1
Alec Jones <alecaej2002@gmail.com>
2016-10-15 16:39:34 -0700

    printing the variables

--- app/helpers/messenger_helper.rb
+++ app/helpers/messenger_helper.rb
@@ -5,7 +5,7 @@ module MessengerHelper
 		if params["hub.verify_token"] == "123456789"
     	render text: params['hub.challenge'] and return
   	else
-    	render text: 'ahhhhhhh, error #{params["hub.verify_token"]}' and return
+    	render text: params["hub.verify_token"] and return
   	end
 	end
 
