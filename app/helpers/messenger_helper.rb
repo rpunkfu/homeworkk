78a6ee62a0891a4414134b6678d674f18d9cf5a4
Alec Jones <alecaej2002@gmail.com>
2016-10-15 16:46:05 -0700

    testing

--- app/helpers/messenger_helper.rb
+++ app/helpers/messenger_helper.rb
@@ -1,8 +1,9 @@
 module MessengerHelper
 
 	def checkFacebookToken
-		puts 'hub.verify_token' + params["hub.verify_token"]
-		if params["hub.verify_token"] == "123456789"
+		verify_token = params["hub.verify_token"]
+		puts verify_token
+		if verify_token == "123456789"
     	render text: params['hub.challenge'] and return
   	else
     	render text: params["hub.verify_token"] and return
