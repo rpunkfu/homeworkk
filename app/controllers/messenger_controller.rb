ca16d266f5826d74eac8c949ebd1465cb6ebf92b
Alec Jones <alecaej2002@gmail.com>
2016-10-15 15:39:06 -0700

    stuff

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -22,6 +22,7 @@ class MessengerController < ApplicationController
 	   body: body,
 	   headers: { 'Content-Type' => 'application/json'},
 	  )
+	  render :nothing => true, status: 200
  	end
 
  	def webhook_inspect
