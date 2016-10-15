1f48ba0becb28baa93cc575e82280d4395faaf7e
Alec Jones <alecaej2002@gmail.com>
2016-10-15 15:35:57 -0700

    removed the status 200

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -4,6 +4,7 @@ class MessengerController < ApplicationController
 	include MessengerHelper
 
 	def receive_message
+		checkFacebookToken()
  		$webhook = JSON.parse(request.raw_post)
  		page_access_token = 'EAAZAjj9YZAiZC0BALu16ZCAkRt33hbnZAqeZBSJgBqjTiEjYz9eP2InIpwbolx7xe8bhfJ9o6oLRTGId6oZCBAbUMoFGgYlZCGVT2NbIcq3dkVAdgw5jh3ZCIT9CDONkx5QSp7nlRcrX5HncCAKwZBBft8UjitoLMgDJXNul7dIzlfbgZDZD'
 
@@ -14,19 +15,13 @@ class MessengerController < ApplicationController
 	   message: {
 	     text: 'this is webhook'
 	   },
-	   status: 200
 	  }.to_json
-	  logger.debug body
+
 	  response = HTTParty.post(
 	   "https://graph.facebook.com/v2.6/me/messages?access_token=#{page_access_token}",
 	   body: body,
 	   headers: { 'Content-Type' => 'application/json'},
 	  )
-	  render status: 200
- 	end
-
- 	def sendtoken
- 		checkFacebookToken()
  	end
 
  	def webhook_inspect
