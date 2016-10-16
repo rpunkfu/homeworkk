c645e5664e48605f3f03efc338bfbe8ba3851b67
Alec Jones <alecaej2002@gmail.com>
2016-10-15 18:01:52 -0700

    inspecting the params

--- app/helpers/messenger_helper.rb
+++ app/helpers/messenger_helper.rb
@@ -1,12 +1,11 @@
 module MessengerHelper
 
 	def checkFacebookToken
-		$verify_token = params["hub.verify_token"]
-		puts 'verify_token' + $verify_token if !$verify_token.nil?
-		if $verify_token == "123456789"
+		verify_token = params["hub.verify_token"]
+		if verify_token == "123456789"
     	render text: params['hub.challenge'] and return
   	else
-    	redirect_to new_group_path
+    	render text: params.inspect
   	end
 	end
 
