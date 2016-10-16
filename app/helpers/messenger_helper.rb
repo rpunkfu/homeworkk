b7bc281901e62136c16b5ae05af4e7f25adcd968
Alec Jones <alecaej2002@gmail.com>
2016-10-15 18:19:41 -0700

    stuff

--- app/helpers/messenger_helper.rb
+++ app/helpers/messenger_helper.rb
@@ -2,10 +2,9 @@ module MessengerHelper
 
 	def checkFacebookToken
 		verify_token = params["hub.verify_token"]
-		puts "params 2 inspect " + params[2].inspect
-		puts "params 1 inspect " + params[1].inspect
-		puts "params 3 inspect " + params[3].inspect
-		puts "params 4 inspect " + params[4].inspect
+		puts "params 3 inspect " + params["hub.challenger"].inspect
+		puts "params 4 inspect " + params["hub.verify_token"].inspect
+		puts "params hub.mode " + params["hub.mode"]
 		if verify_token == "123456789"
     	render text: params['hub.challenge'] and return
   	else
