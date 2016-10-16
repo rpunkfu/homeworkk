7d4855230afa0c4c717126e4192d6a263920b47a
Alec Jones <alecaej2002@gmail.com>
2016-10-16 10:48:21 -0700

    testing out messaging in a class

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -6,20 +6,7 @@ class MessengerController < ApplicationController
 	def receive_message
 		checkFacebookToken()
  		$webhook = JSON.parse(request.raw_post)
- 		page_access_token = 'EAAZAjj9YZAiZC0BALu16ZCAkRt33hbnZAqeZBSJgBqjTiEjYz9eP2InIpwbolx7xe8bhfJ9o6oLRTGId6oZCBAbUMoFGgYlZCGVT2NbIcq3dkVAdgw5jh3ZCIT9CDONkx5QSp7nlRcrX5HncCAKwZBBft8UjitoLMgDJXNul7dIzlfbgZDZD'
- 		body = {
- 			recipient: {
-   			id: '134381003642835'
- 			},
- 			message: {
-   			text: 'it texts'
- 			}
-		}.to_json
-		response = HTTParty.post(
- 			"https://graph.facebook.com/v2.6/me/messages?access_token=#{page_access_token}",
- 			body: body,
- 			headers: { 'Content-Type' => 'application/json' }
-		)
+ 		Messagehuman.sendMessage()
  	end
 
  	def check_token
--- app/models/messagehuman.rb
+++ app/models/messagehuman.rb
@@ -1,3 +1,18 @@
 class Messagehuman
-
+	def self.sendMessage()
+		page_access_token = 'EAAZAjj9YZAiZC0BALu16ZCAkRt33hbnZAqeZBSJgBqjTiEjYz9eP2InIpwbolx7xe8bhfJ9o6oLRTGId6oZCBAbUMoFGgYlZCGVT2NbIcq3dkVAdgw5jh3ZCIT9CDONkx5QSp7nlRcrX5HncCAKwZBBft8UjitoLMgDJXNul7dIzlfbgZDZD'
+ 		body = {
+ 			recipient: {
+   			id: '134381003642835'
+ 			},
+ 			message: {
+   			text: 'heyo aleco'
+ 			}
+		}.to_json
+		response = HTTParty.post(
+ 			"https://graph.facebook.com/v2.6/me/messages?access_token=#{page_access_token}",
+ 			body: body,
+ 			headers: { 'Content-Type' => 'application/json' }
+		)
+	end
 end
