47b9a81e21816199b0750e906910646bc0833426
Alec Jones <alecaej2002@gmail.com>
2016-10-16 10:56:05 -0700

    trying to get id from webhook

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -7,6 +7,8 @@ class MessengerController < ApplicationController
 		checkFacebookToken()
  		$webhook = JSON.parse(request.raw_post)
  		Messagehuman.sendMessage('134381003642835', 'testing using params')
+ 		$recipient = $webhook["entry"][0]["messaging"][0]["sender"]
+ 		$recipent1 = $webhook["entry"]["messaging"]["sender"]
  	end
 
  	def check_token
