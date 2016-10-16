6b7adfdc354f31ee7b1d0079955676507fcd98cc
Alec Jones <alecaej2002@gmail.com>
2016-10-16 16:00:21 -0700

    stuff

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -8,8 +8,8 @@ class MessengerController < ApplicationController
  		$webhook = JSON.parse(request.raw_post)
  		recipient = $webhook["entry"][0]["messaging"][0]["sender"]["id"]
  		Messagehuman.checkUserExists(recipient)
- 		$stuffy = @messageText
- 		Messagehuman.sendMessage(recipient, @messageText)
+ 		$stuffy = messageText
+ 		Messagehuman.sendMessage(recipient, messageText)
  	end
 
  	def check_token
--- app/models/messagehuman.rb
+++ app/models/messagehuman.rb
@@ -29,11 +29,11 @@ class Messagehuman
 
 	def self.checkUserExists(recipient)
 		if User.find_by(conversation_id: recipient.to_s).nil?
- 			@messageText = 'sign up for christopherbot here: https://christopherbot.herokuapp.com/users/sign_in/?conversation_id=#{recipient}'
- 			return @messageText
+ 			messageText = 'sign up for christopherbot here: https://christopherbot.herokuapp.com/users/sign_in/?conversation_id=#{recipient}'
+ 			return messageText
  		else
- 			@messageText = 'you are already signed up'
- 			return @messageText
+ 			messageText = 'you are already signed up'
+ 			return messageText
  		end
  	end
 			
