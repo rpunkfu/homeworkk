db951f345c8e96e9f923c642f6fa0022b09602e6
Alec Jones <alecaej2002@gmail.com>
2016-10-15 18:06:19 -0700

    trying to inspect vars

--- app/helpers/messenger_helper.rb
+++ app/helpers/messenger_helper.rb
@@ -2,10 +2,11 @@ module MessengerHelper
 
 	def checkFacebookToken
 		verify_token = params["hub.verify_token"]
+		puts "params inspect " + params.inspect
 		if verify_token == "123456789"
     	render text: params['hub.challenge'] and return
   	else
-    	render text: params.inspect
+    	render text: params.inspect + verify_token
   	end
 	end
 
