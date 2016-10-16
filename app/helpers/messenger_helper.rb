92adaae2ab15e975de5543fb213395295d671dbe
Alec Jones <alecaej2002@gmail.com>
2016-10-15 17:58:41 -0700

    testing out the verify token

--- app/helpers/messenger_helper.rb
+++ app/helpers/messenger_helper.rb
@@ -1,12 +1,12 @@
 module MessengerHelper
 
 	def checkFacebookToken
-		@verify_token = params["hub.verify_token"]
-		puts 'verify_token' + @verify_token if !@verify_token.nil?
-		if @verify_token == "123456789"
+		$verify_token = params["hub.verify_token"]
+		puts 'verify_token' + $verify_token if !$verify_token.nil?
+		if $verify_token == "123456789"
     	render text: params['hub.challenge'] and return
   	else
-    	render text: 'sorry, it failed' and return
+    	redirect_to new_group_path
   	end
 	end
 
