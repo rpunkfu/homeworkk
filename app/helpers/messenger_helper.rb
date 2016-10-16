118f546b9f8cbe51eb12d2edcbbebe10c67e2a82
Alec Jones <alecaej2002@gmail.com>
2016-10-15 18:20:49 -0700

    stuff

--- app/helpers/messenger_helper.rb
+++ app/helpers/messenger_helper.rb
@@ -4,7 +4,7 @@ module MessengerHelper
 		verify_token = params["hub.verify_token"]
 		puts "params 3 inspect " + params["hub.challenger"].inspect
 		puts "params 4 inspect " + params["hub.verify_token"].inspect
-		puts "params hub.mode " + params["hub.mode"]
+		puts "params hub.mode " + params["hub.mode"].inspect
 		if verify_token == "123456789"
     	render text: params['hub.challenge'] and return
   	else
