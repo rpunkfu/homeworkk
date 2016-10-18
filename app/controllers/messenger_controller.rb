e0467d7362c7692439e741b966e325dcee7f4922
Alec Jones <alecaej2002@gmail.com>
2016-10-17 17:13:50 -0700

    stuff

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -6,12 +6,13 @@ class MessengerController < ApplicationController
 	def receive_message
 		checkFacebookToken()
  		$webhook = JSON.parse(request.raw_post)
- 		recipient = $webhook["entry"][0]["messaging"][0]["sender"]["id"]
- 		userText = $webhook["entry"][0]["messaging"][0]["message"]["text"]
- 		$messageText = Messagehuman.checkKeyWords(userText) # grabs message to send from function
+ 		@recipient = $webhook["entry"][0]["messaging"][0]["sender"]["id"]
+ 		@userText = $webhook["entry"][0]["messaging"][0]["message"]["text"]
+ 		
+ 		@messageText = Messagehuman.checkKeyWords(userText) # grabs message to send from function
  		# write if statement so if there is a text outputted, it will send that text and then break from the if statement
 
- 		Messagehuman.sendMessage(recipient, messageText)
+ 		Messagehuman.sendMessage(@recipient, @messageText)
  	end
 
  	def check_token
