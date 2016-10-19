7472aa807723d2829b6b169d475d3977ef9eba68
Alec Jones <alecaej2002@gmail.com>
2016-10-19 16:52:46 -0700

    aww look its my favourite patient

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -68,7 +68,7 @@ class GroupsController < ApplicationController
     if !params[:group].nil?
       params[:group].each do |group|
         next if group[:group_name].blank? || group[:end_time].blank?
-        @group = current_user.groups.build(name: group[:group_name], end_time: group[:end_time], group_day: group[:group_day], conversation_id: group[:conversation_id])
+        @group = current_user.groups.build(name: group[:group_name], end_time: group[:end_time].utc, group_day: group[:group_day], conversation_id: group[:conversation_id])
         @group.save
         counter += 1
       end
@@ -89,7 +89,7 @@ class GroupsController < ApplicationController
       params[:group].each do |group|
         if groupCounter > $groupUpdateNumber
           break if group[:group_name].blank? || group[:end_time].blank?
-          @group = current_user.groups.build(name: group[:group_name], end_time: group[:end_time], group_day: group[:group_day], conversation_id: group[:conversation_id])
+          @group = current_user.groups.build(name: group[:group_name], end_time: group[:end_time].utc, group_day: group[:group_day], conversation_id: group[:conversation_id])
           @group.save
         end
         next if group[:group_name].blank? || group[:end_time].blank?
