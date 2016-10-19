b487f8ad26cf16bfc6c1b88bf8dc836d83db0a07
Alec Jones <alecaej2002@gmail.com>
2016-10-19 16:33:59 -0700

    trying to test out scheduler

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161019152620) do
+ActiveRecord::Schema.define(version: 20161019153536) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20161019152620) do
     t.string  "homework_assignment"
     t.string  "conversation_id"
     t.string  "group_name"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
--- lib/tasks/scheduler.rake
+++ lib/tasks/scheduler.rake
@@ -1,5 +1,14 @@
 desc "This task is called by the Heroku scheduler add-on"
 
 task :message_task => :environment do
+	@groups = Group.all 
+	@t = Time.now.utc.strftime("%H:%M:%S")
+	@timeten = 10.minutes.from_now.utc.strftime("%H:%M:%S")
+
+	@groups.each do |group|
+		if group.end_time.utc.strftime("%H:%M:%S") >= @t && group.end_time.utc.strftime("%H:%M:%S") <= @timeten
+			Messagehuman.sendMessage(group.conversation_id, group.group_name)
+		end
+	end
 
 end
