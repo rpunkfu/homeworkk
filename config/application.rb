129aad5868aab90e0a8d1e148ce4f0438c670e60
Alec Jones <alecaej2002@gmail.com>
2016-10-19 17:01:05 -0700

    set the time zone

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,9 @@ module Homeworkk
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    
+    config.time_zone = "Pacific Time (US & Canada)"
+    config.active_record.default_timezone = :local
   end
 end
