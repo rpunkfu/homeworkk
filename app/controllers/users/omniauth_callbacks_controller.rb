30a0afcb43a86837f592bd487bf88d1af74a5160
Alec Jones <alecaej2002@gmail.com>
2016-11-13 14:55:27 -0800

    added talk to christopher bot page

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,7 +6,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     if @user.conversation_id.nil?
       @existingUser = User.find_by(uid: @user.uid)
       if @existingUser.conversation_id.nil?
-        redirect_to root_path, notice: "You need to talk to Christopher"
+        redirect_to pages_talk_to_christopher_path
       else
 
     if @user.persisted?
