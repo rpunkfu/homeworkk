8cef5b72b34c1e0d1d08a40ccd423777d1460217
Alec Jones <alecaej2002@gmail.com>
2016-11-14 12:43:33 -0800

    annyoing

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -10,8 +10,8 @@ class MessengerController < ApplicationController
  		@userText = $webhook["entry"][0]["messaging"][0]["message"]["text"].downcase
  		currentClasses = Grouparray.all
 
- 		@checkUserExists = Messagehuman.checkUserExists(@recipient)
- 		Messagehuman.sendButton(@recipient) if User.find_by(conversation_id: @recipient).nil? && @checkUserExists == false
+ 		#@checkUserExists = Messagehuman.checkUserExists(@recipient)
+ 		#Messagehuman.sendButton(@recipient) if User.find_by(conversation_id: @recipient).nil? && @checkUserExists == false
  		#@checkUserExists = Messagehuman.checkUserExists(@recipient)
  		#Messagehuman.sendMessage(@recipient, @checkUserExists)
 
