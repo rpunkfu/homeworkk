d197e6c2f3d87d9dee3e5761abc7778e70704a9d
Alec Jones <alecaej2002@gmail.com>
2016-11-14 13:02:07 -0800

    ahah

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -8,6 +8,8 @@ class MessengerController < ApplicationController
  		$webhook = JSON.parse(request.raw_post)
  		@recipient = $webhook["entry"][0]["messaging"][0]["sender"]["id"]
  		@userText = $webhook["entry"][0]["messaging"][0]["message"]["text"].downcase
+ 		@positiveResponses = ["thats grrrreaat", "Thats Awesome!", "Yay! No Homework!", "Finally, a break from some homework", "Awesome. Just what i needed to hear.", "Yay. Some good news today.", "thats almost better than harry potter", "time to celebrate, come on!"]
+		@negativeResponses = ["booooo.", "what a shame." "ugh. That stinks.", "your teacher needs to chill out on the homework", "That's so sad to hear", "that sucks, at least you look good today.", "that sucks more than a vacuum", "thats worse than when Dumbledore died."]
  		currentClasses = Grouparray.all
 
  		#@checkUserExists = Messagehuman.checkUserExists(@recipient)
@@ -19,13 +21,11 @@ class MessengerController < ApplicationController
  			randomNum = rand(0..7)
  			if group.conversation_id == @recipient
  				if @userText == "yes"
-=begin
  					Messagehuman.sendMessage(group.conversation_id, @negativeResponses[randomNum])
  					@groupArrayGroup = Grouparray.find_by(id: group.id)
  					@groupArrayGroup.destroy
  					@group = Group.find_by(conversation_id: group.conversation_id, group_name: group.group_name, group_day: group.group_day, end_time: group.end_time)
  					@group.update(homework_assigned: true)
-=end
  				elsif @userText == "no"
 =begin
  					Messagehuman.sendMessage(group.conversation_id, @positiveResponses[randomNum])
