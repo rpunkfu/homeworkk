33bd78f8411f471881e27dd30c7d23f8fa4c04f6
Alec Jones <alecaej2002@gmail.com>
2016-11-20 10:15:52 -0800

    stuff

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,27 +3,15 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     
-    @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
-    @facebookUser = @user
-
-    if $conversation_id.nil?
-      @existingUser = User.find_by(uid: @facebookUser.uid)
-      puts "THIS IS LINE 10"
-      if @existingUser.nil?
-        puts "REDIRECTING THE USER"
-        redirect_to pages_talk_to_christopher_path
-      end
-      puts "THIS IS LINE 15"
+    @user = request.env["omniauth.auth"]
+    
+    if @user.persisted?
+      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+    else
+      session["devise.facebook_data"] = request.env["omniauth.auth"]
+      redirect_to root_path if current_user.nil?
     end
-      puts "THIS IS LINE 17"
-
-      if @user.persisted?
-        sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-        set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
-      else
-        session["devise.facebook_data"] = request.env["omniauth.auth"]
-        redirect_to root_path if current_user.nil?
-      end
   end
 
 
