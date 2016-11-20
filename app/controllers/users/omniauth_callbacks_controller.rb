2907f3c7a90ffddde239d2e2d46a703dd7934560
Alec Jones <alecaej2002@gmail.com>
2016-11-20 09:57:42 -0800

    trying to fix logic

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,6 +4,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     
     @facebookUser = request.env["omniauth.auth"]
+    @user = request.env["omniauth.auth"]
 
     if $conversation_id.nil?
       @existingUser = User.find_by(uid: @facebookUser.uid)
@@ -13,7 +14,14 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
         redirect_to pages_talk_to_christopher_path
       end
       puts "THIS IS LINE 15"
-    end
+      if @facebookUser.persisted?
+        sign_in_and_redirect @facebookUser, :event => :authentication #this will throw if @user is not activated
+        set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+      else
+        session["devise.facebook_data"] = request.env["omniauth.auth"]
+        redirect_to new_user_registration_url if current_user.nil?
+      end
+    else
       puts "THIS IS LINE 17"
       @user = @facebookUser
 
