dd6cc7b9fc6c30e65bf0bf6de2f88cf4c7a61e84
Alec Jones <alecaej2002@gmail.com>
2016-11-20 10:06:04 -0800

    i dont know

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,8 +3,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     
-    @facebookUser = request.env["omniauth.auth"]
     @user = request.env["omniauth.auth"]
+    @facebookUser = @user
 
     if $conversation_id.nil?
       @existingUser = User.find_by(uid: @facebookUser.uid)
@@ -17,8 +17,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
       puts "THIS IS LINE 17"
 
-      if @facebookUser.persisted?
-        sign_in_and_redirect @facebookUser, :event => :authentication #this will throw if @user is not activated
+      if @user.persisted?
+        sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
         set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
       else
         session["devise.facebook_data"] = request.env["omniauth.auth"]
