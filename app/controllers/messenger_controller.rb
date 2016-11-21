00b7ddd84323e613a452a92556b7f6c6ea0435ce
Alec Jones <alecaej2002@gmail.com>
2016-11-21 09:52:08 -0800

    stuff

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -22,19 +22,19 @@ class MessengerController < ApplicationController
  			if group.conversation_id == @recipient
  				if @userText == "yes"
  					@group = Group.find_by(conversation_id: group.conversation_id, group_name: group.group_name, group_day: group.group_day, end_time: group.end_time)
- 					@group.update(homework_assigned: true)
+ 					@group.update(homework_assigned: true) if !@group.nil?
  					@grouparray = Grouparray.find_by(id: group.id)
- 					@grouparray.update(homework_assigned: true)
+ 					@grouparray.update(homework_assigned: true) if !@groupArray.nil?
  					Messagehuman.sendMessage(group.conversation_id, 'what homework do you have?')
  				elsif @userText == "no"
  					Messagehuman.sendMessage(group.conversation_id, @positiveResponses[randomNum])
  					@groupArrayGroup = Grouparray.find_by(id: group.id)
  					@groupArrayGroup.destroy
  					@group = Group.find_by(conversation_id: group.conversation_id, group_name: group.group_name, group_day: group.group_day, end_time: group.end_time)
- 					@group.update(homework_assigned: false)
+ 					@group.update(homework_assigned: false) if !@group.nil?
  				elsif group.homework_assigned == true
  					@group = Group.find_by(conversation_id: group.conversation_id, group_name: group.group_name, group_day: group.group_day, end_time: group.end_time)
- 					@group.update(homework_assignment: @userText)
+ 					@group.update(homework_assignment: @userText) if !@group.nil?
  					@groupArray = Grouparray.find_by(id: group.id)
  					@groupArray.destroy
  				else
