ede835b626a76a48df2e41fad428063ac7fb2f96
Alec Jones <alecaej2002@gmail.com>
2016-11-27 11:15:49 -0800

    stuff

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -58,7 +58,7 @@ class GroupsController < ApplicationController
 
   def checkDayHomework
     daysOfWeek = [["monday", 0],["tuesday",1],["wednesday",2],["thursday",3],["friday",4]]
-    daysOfWeek.each do
+    daysOfWeek.each do |day|
       if current_user.groups.where("group_day = ?", day[0]).nil?
         return day
         exit!
