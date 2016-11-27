6e060fdaef8fae519b17bed5ef3c32b1dcdc52b9
Alec Jones <alecaej2002@gmail.com>
2016-11-27 10:36:37 -0800

    stuff

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -56,6 +56,18 @@ class GroupsController < ApplicationController
   end
   helper_method :checkIfHomework
 
+  def checkDayHomework
+    daysOfWeek = [["monday", 0],["tuesday",1],["wednesday",2],["thursday",3],["friday",4]]
+    counter = 0
+    daysOfWeek.each do
+      if current_user.groups.where("group_day = ?", daysOfWeek[counter][0]).nil?
+        return daysOfWeek[counter]
+        counter += 1
+      end
+    end
+  end
+  helper_method :checkDayHomework
+
 
   # GET /groups/1
   # GET /groups/1.json
@@ -65,29 +77,9 @@ class GroupsController < ApplicationController
 
   # GET /groups/new
   def new
-    @classSignUpDay = "monday" # day your class is for
-    @group = current_user.groups.build  # so you can create new classes
-    @groups = current_user.groups # list of the user's groups
-    @groups.each do |group| # if the group day exists, then set @classSignUpDay to the next
-      case group.group_day
-        when "monday"
-          @yesterdayClassDay = "monday"
-          @classSignUpDay = "tuesday"
-        when "tuesday"
-          @yesterdayClassDay = "tuesday"
-          @classSignUpDay = "wednesday"
-        when "wednesday"
-          @yesterdayClassDay = "wednesday"
-          @classSignUpDay = "thursday"
-        when "thursday"
-          @yesterdayClassDay = "thursday"
-          @classSignUpDay = "friday"
-        when "friday"
-          redirect_to root_path, notice: "You have already set up all your classes, edit them instead"
-          break
-        else
-      end
-    end 
+    daysOfWeek = ["monday", "tuesday", "wednesday", "thursday", "friday"]
+    @classSignUpDay = checkDayHomework
+    @yesterdayClassDay = 0
     @yesterday_groups = current_user.groups.where("group_day = ?", @yesterdayClassDay).order("end_time ASC") # groups for set from yesterday
   end
 
