c06e13fe1534b95659e9c9e1a2f4057e9f0f64cc
Alec Jones <alecaej2002@gmail.com>
2016-11-27 17:35:06 -0800

    stuff

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -263,10 +263,7 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
 
-  #for facebook authentication
-  if Rails.env.production?
+
     config.omniauth :facebook, "1894774840756659", "7e1cb8536d22cef46f21f8d2b705cd43", scope: 'email,public_profile', info_fields: 'email, first_name, last_name'
-  else  
-    config.omniauth :facebook, "1798319283735549", "f228177f7baee22f1b9e158a19749dfd", scope: 'email,public_profile', info_fields: 'email, first_name, last_name'
-  end
+
 end
