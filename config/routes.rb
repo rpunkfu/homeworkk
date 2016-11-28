0667b452dd3bb6f90663fe5dc6bd0585c2413ff2
Alec Jones <alecaej2002@gmail.com>
2016-11-27 18:18:32 -0800

    removed session from routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :groups
 
-  devise_for :users, :controllers => {registrations: 'registrations', sessions: "users/sessions", :omniauth_callbacks => "users/omniauth_callbacks"}
+  devise_for :users, :controllers => {registrations: 'registrations', :omniauth_callbacks => "users/omniauth_callbacks"}
   root 'groups#index'
+  # sessions: "users/sessions"
 
   post 'messenger/webhook' => 'messenger#receive_message'
   get 'messenger/webhook' => 'messenger#check_token'
