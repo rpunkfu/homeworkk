f7480d2a5bb01aa9d10c7b7d71d501d38bf533a8
Alec Jones <alecaej2002@gmail.com>
2016-12-06 17:36:08 -0800

    trying to fix that get started bug

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -8,8 +8,12 @@ class MessengerController < ApplicationController
  		$webhook = JSON.parse(request.raw_post)
  		@recipient = $webhook["entry"][0]["messaging"][0]["sender"]["id"]
  		@userText = $webhook["entry"][0]["messaging"][0]["message"]["text"].downcase if !$webhook["entry"][0]["messaging"][0]["message"]["text"].nil?
- 		$userStartText = $webhook["entry"][0]["messaging"][0]["postback"]["payload"] if !$webhook["entry"][0]["messaging"][0]["postback"]["payload"].nil? && @userText.nil?
- 		$userStartRecipient = $webhook["entry"][0]["messaging"][0]["sender"]["id"] if !$webhook["entry"][0]["messaging"][0]["sender"]["id"].nil?
+ 		
+
+ 		$userStartText = $webhook["entry"][0]["messaging"][0]["recipient"]["postback"]["payload"] if !$webhook["entry"][0]["messaging"][0]["postback"]["payload"].nil? && @userText.nil?
+ 		$userStartRecipient = $webhook["entry"][0]["messaging"][0]["recipient"]["sender"]["id"] if !$webhook["entry"][0]["messaging"][0]["sender"]["id"].nil?
+ 		
+
  		@positiveResponses = ["thats grrrreaat", "Thats Awesome!", "Yay! No Homework!", "Finally, a break from some homework", "Awesome. Just what i needed to hear.", "Yay. Some good news today.", "thats almost better than harry potter", "time to celebrate, come on!"]
 		@negativeResponses = ["booooo.", "what a shame." "ugh. That stinks.", "your teacher needs to chill out on the homework", "That's so sad to hear", "that sucks, at least you look good today.", "that sucks more than a vacuum", "thats worse than when Dumbledore died."]
 		@defaultResponses = ["Hey! You've already signed up. All you have to do is wait for me to text you"]
