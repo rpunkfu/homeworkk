575124cd5a0535d7a156510b7e61a0146ff9b0d2
Alec Jones <alecaej2002@gmail.com>
2016-12-06 17:48:23 -0800

    please have fixed the get started problem

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -10,7 +10,7 @@ class MessengerController < ApplicationController
  		@userText = $webhook["entry"][0]["messaging"][0]["message"]["text"].downcase if !$webhook["entry"][0]["messaging"][0]["message"]["text"].nil?
 
  		@userStartText = $webhook["entry"][0]["messaging"][0]["postback"]["payload"] if !$webhook["entry"][0]["messaging"][0]["postback"]["payload"].nil? && @userText.nil?
- 		@userStartRecipient = $webhook["entry"][0]["messaging"][0]["recipient"]["id"] if @recipient.nil? && !$webhook["entry"][0]["messaging"][0]["recipient"]["id"].nil?
+ 		@userStartRecipient = $webhook["entry"][0]["messaging"][0]["sender"]["id"] if @recipient.nil? && !$webhook["entry"][0]["messaging"][0]["recipient"]["id"].nil?
  		
  		@positiveResponses = ["thats grrrreaat", "Thats Awesome!", "Yay! No Homework!", "Finally, a break from some homework", "Awesome. Just what i needed to hear.", "Yay. Some good news today.", "thats almost better than harry potter", "time to celebrate, come on!"]
 		@negativeResponses = ["booooo.", "what a shame." "ugh. That stinks.", "your teacher needs to chill out on the homework", "That's so sad to hear", "that sucks, at least you look good today.", "that sucks more than a vacuum", "thats worse than when Dumbledore died."]
