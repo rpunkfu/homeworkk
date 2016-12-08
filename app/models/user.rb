66c1aab89a75b6408c414c59356d8c4370b4ae6c
Alec Jones <alecaej2002@gmail.com>
2016-12-07 17:58:55 -0800

    fixed one of the bugs for conversation_id not working

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,12 +14,12 @@ class User < ActiveRecord::Base
     false
   end
 
-  def self.from_omniauth(auth)
+  def self.from_omniauth(auth, conversation_id)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.first_name = auth.info.first_name
       user.provider = auth.provider
       user.uid = auth.uid
-      #user.conversation_id = conversation_id.to_s
+      user.conversation_id = conversation_id.to_s
       user.password = Devise.friendly_token[0,20]
     end
   end
