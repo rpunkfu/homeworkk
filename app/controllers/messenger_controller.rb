13e30915bc685de4fe7fc25b95f0ece2263a561d
Alec Jones <alecaej2002@gmail.com>
2016-12-10 15:43:09 -0800

    trying to fix the messenger controller

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -55,7 +55,7 @@ class MessengerController < ApplicationController
 	 		end
  		end
 
- 		if !@groupsResponse.nil? || !groupsResponse.nil?
+ 		if !@groupsResponse.nil? && !@groupsResponse.empty?
 	 			@groupsResponse.each do |group|
 	 				puts "IN GROUP RESPONSE"
 	 				if group.group_name == @userText
