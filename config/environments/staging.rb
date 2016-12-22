d992a6cd42138473418bae13a4302bf287f04db1
Alec Jones <alecaej2002@gmail.com>
2016-12-22 14:30:27 -0800

    testing out stuff

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -3,9 +3,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  
-  RACK_ENV=staging 
-  RAILS_ENV=staging
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
