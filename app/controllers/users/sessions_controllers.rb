96fb14ea7852a3be7d3cfd5fc5f7669a916de930
Alec Jones <alecaej2002@gmail.com>
2016-12-27 09:56:27 -0800

    trying to fix sign in

--- app/controllers/users/sessions_controllers.rb
+++ app/controllers/users/sessions_controllers.rb
@@ -1,5 +1,9 @@
 class Users::SessionsController < Devise::SessionsController
   def new
-
+  	if params.to_a[0][0] == "conversation_id" 
+	 		$conversation_id = params.to_a[0][1] 
+ 		else 
+			$conversation_id = nil 
+ 		end 
   end
 end
