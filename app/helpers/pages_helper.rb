8c0ce62c31009bb4b50d703a02ce942f9ff4f54a
Alec Jones <alecaej2002@gmail.com>
2017-01-28 21:36:53 -0800

    fixed sign up bug

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,13 +1,17 @@
 module PagesHelper
 	def resource_name
-    :user
-  end
+       :user
+     end
 
-  def resource
-   
-  end
+     def resource_class 
+        User 
+     end
 
-  def devise_mapping
-    @devise_mapping ||= Devise.mappings[:user]
-  end
+     def resource
+       @resource ||= User.new
+     end
+
+     def devise_mapping
+       @devise_mapping ||= Devise.mappings[:user]
+     end
 end
