c19896faf4888087f948cb9f2cc0d63fb330f447
Alec Jones <alecaej2002@gmail.com>
2017-02-07 19:20:04 -0800

    trying to fix redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,18 @@
 class ApplicationController < ActionController::Base
+	PREFERED_HOST = 'https://www.christopherbot.co/'
+	before_filter :redirect_to_prefered_host
+
 
   protected
+
   def set_user_api_token
     RequestStore.store[:my_api_token] = current_user.api_token # or something similar based on `session`
   end
+
+  def redirect_to_prefered_host
+  	if Rails.env.production? && request.host != PREFERED_HOST
+    	redirect_to(PREFERED_HOST)
+  	end
+	end
 
 end
