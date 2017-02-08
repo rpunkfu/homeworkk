ecae5b517e3e18e672f04cc6d1675e68a573e552
Alec Jones <alecaej2002@gmail.com>
2017-02-07 19:09:12 -0800

    reverted back redirects

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,4 @@ class ApplicationController < ActionController::Base
     RequestStore.store[:my_api_token] = current_user.api_token # or something similar based on `session`
   end
 
-  def redirectCB(url)
-  	if url == "http://christopherbot.herokuapp.com/users/sign_in" || url == "https://christopherbot.herokuapp.com/users/sign_in"
-  		redirect_to "https://www.christopherbot.co/users/sign_in/"
-  	end
-  end
-  helper_method :redirectCB
 end
