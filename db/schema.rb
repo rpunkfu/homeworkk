3f5b85c4214ec57dabc00cccb4ed2762b88abc01
Alec Jones <alecaej2002@gmail.com>
2017-02-13 14:36:51 -0800

    removed secret files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161229052811) do
+ActiveRecord::Schema.define(version: 20170212174520) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
